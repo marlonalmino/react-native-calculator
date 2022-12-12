ed08a8ebc1e813a55233e50c8b0ab4de86e68126
Marlon Almino <alminomarlon@gmail.com>
2022-12-12 20:01:17 -0300

    Correções

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
   state = { ...initialState }
 
   addDigit = n => {
-    if (n === '.' && this.state.displayValue.includes('.')) {
+    if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
       return
     }
 
@@ -56,7 +56,7 @@ export default class App extends Component {
 
       values[1] = 0
       this.setState({
-        displayValue: values[0],
+        displayValue: `${values[0]}`,
         operation: equals ? null : operation,
         current: equals ? 0 : 1,
         clearDisplay: !equals,
